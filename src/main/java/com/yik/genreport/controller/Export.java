5833019b4b7139b452d1bddcd05e8c082969a1c1
YMM <1356992767@qq.com>
2022-04-29 11:43:13 +0800

    初次提交

---
+++ src/main/java/com/yik/genreport/controller/Export.java
@@ -0,0 +1,14 @@
+package com.yik.genreport.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: wangtq
+ * @Date: 2022/04/29/11:42
+ * @Description:
+ */
+@Controller
+public class Export {
+}

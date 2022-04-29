df4d2e3cccf991adfb8d6c1ae2dc06fd275df9b2
YMM <1356992767@qq.com>
2022-04-29 11:04:39 +0800

    初次提交

---
+++ src/main/java/com/yik/genreport/GenReportApplication.java
@@ -0,0 +1,13 @@
+package com.yik.genreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenReportApplication.class, args);
+    }
+
+}
